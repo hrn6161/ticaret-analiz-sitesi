5af10349ecdc263e3255cb57cf1946865a723fa2
Harun Birinci <harunbirinci@Harun-MacBook-Air.local>
2025-10-07 14:51:46 +0300

    Render için production ayarları

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def check_status(file_id):
         return jsonify({'ready': False})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
